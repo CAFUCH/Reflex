0098b4459c5d9b36e7ee02c986a75e85f24668f1
CAFUCH <98962616+CAFUCH@users.noreply.github.com>
2022-06-09 19:03:50 +0900

    씬 이동

---
+++ Assets/01.Script/Dev/SeolAh/StageMove.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageMove : MonoBehaviour
+{
+    //문 진입하면 페이드 인 아웃
+    //스테이지 이동(프리팹 갈아끼우기? - GameM.StageManager(프리팹)을 바꾸는?)
+    //플레이어 스폰 지점 설정(player.transform?)
+    //진입 문구 띄우기 2초(천천히  사라지기)
+}
